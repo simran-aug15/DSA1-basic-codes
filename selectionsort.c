c3e3791627d375c50d6f5542ef3713cd5254eadd
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2025-11-19 23:22:31 +0530

    Added file selectionsort.c

---
+++ selectionsort.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int selectionsort(int arr[],int size)
+{  int min ,i,temp;
+    for(int i=0;i<size-1;i++)
+    {
+        int min=i;
+        for(int j=i+1;j<size;j++)
+        {
+            if(arr[j]<arr[min])
+            {
+                min=j;
+            }
+        }
+    
+        int temp=arr[i];
+        arr[i]=arr[min];
+        arr[min]=temp;
+    }
+}
+
+    
+    int main()
+    {
+        int arr[]={2,56,34,78,12};
+        int size=sizeof(arr)/sizeof(arr[0]);
+        selectionsort(arr,size);
+        for(int i=0;i<size;i++)
+        {
+            printf("%d  ",arr[i]);
+        }
+
+    }
+
