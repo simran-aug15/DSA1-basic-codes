dbfbaa126635784b6d11165ca31b7bf38eac2dea
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2025-11-19 23:22:30 +0530

    Added file insertionsort.c

---
+++ insertionsort.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int insertionsort(int arr[],int size)
+{
+    int i,j,key;
+    for(i=1;i<size;i++)
+    {
+        key=arr[i];
+        j=i-1;
+        while(j>=0 && arr[j]>key)
+        {
+            arr[j+1]=arr[j];
+            j=j-1;
+        }
+        arr[j+1]=key;
+    }
+}
+int main()
+    {
+        int arr[]={2,56,34,78,12};
+        int size=sizeof(arr)/sizeof(arr[0]);
+        insertionsort(arr,size);
+        for(int i=0;i<size;i++)
+        {
+            printf("%d  ",arr[i]);
+        }
+
+    }
