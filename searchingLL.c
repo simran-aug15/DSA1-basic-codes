e3df676ea05a4f1d016976c6e53578ac9f400e73
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2025-11-19 23:22:31 +0530

    Added file searchingLL.c

---
+++ searchingLL.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include<stdlib.h>
+//searching a value  of single LL
+struct node
+{
+  int data;
+  struct node*next;  /* data */
+};
+ void traverselinkedlist(struct node*ptr)
+{
+    while(ptr!=NULL)
+    {
+        printf("The values are: %d",ptr->data);
+        ptr=ptr->next;
+        printf("\n");
+    }
+}
+struct node* search(struct node*head,int value)
+{
+   struct node*p=head;
+   int pos=1;
+   while( p->next!=NULL)
+   {
+    if(p->data == value)
+    {
+        printf("Element %d found at pos %d",value,pos);
+        return head;
+    }
+    p=p->next;
+    pos++;
+   }
+  printf("Element %d  not found ",value);
+
+
+
+   
+}
+int main()
+{
+   struct node*head=(struct node*)malloc(sizeof(struct node));
+    struct node*second=(struct node*)malloc(sizeof(struct node));
+    struct node*third=(struct node*)malloc(sizeof(struct node));
+    head->data=7;
+    head->next=second;
+    second->data=12;
+    second->next=third;
+
+    third->data=70;
+    third->next=NULL;
+    printf("original \n");
+    traverselinkedlist(head);
+    head=search(head,10);
+
+     
+    
+}
+
+
