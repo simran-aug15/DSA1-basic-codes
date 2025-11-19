5bf1938468cf8e0e092536b165e63a217955ea6d
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2025-11-19 23:22:28 +0530

    Added file circularLLatend.c

---
+++ circularLLatend.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include<stdlib.h>
+//Insertion at end of  cicular LL
+struct node
+{
+  int data;
+  struct node*next;  /* data */
+};
+ void traverselinkedlist(struct node*head)
+{  struct node*ptr=head;
+    do
+    {
+        printf("The values are: %d",ptr->data);
+        ptr=ptr->next;
+        printf("\n");
+    }
+    while(ptr!=head);
+
+}
+struct node* insertionatend(struct node*head,int data)
+{
+   struct node*ptr=(struct node*)malloc(sizeof(struct node ));
+   struct node*p=head->next;
+   ptr->data=data;
+   while(p->next!=head)
+   {
+    p=p->next;
+   }
+   p->next=ptr;
+   ptr->next=head;
+    
+
+   return head;
+}
+int main()
+{
+   struct node*head=(struct node*)malloc(sizeof(struct node));
+    struct node*second=(struct node*)malloc(sizeof(struct node));
+    struct node*third=(struct node*)malloc(sizeof(struct node));
+    head->data=7;
+    head->next=second;
+    second->data=73;
+   second->next=third;
+
+    third->data=70;
+    third->next=head;
+    printf("original \n");
+    traverselinkedlist(head);
+    head=insertionatend(head,569);
+    printf("after insertion \n");
+       traverselinkedlist(head);
+     
+    
+}
+
+
+
+
