1a9ce8fe4419ee74c3d5d232ea80360ac1570452
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2025-11-21 18:20:59 +0530

    Some changes

---
+++ 3.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+    int data;
+    struct node* next;
+};
+struct node* f=NULL;
+struct node* r=NULL;
+void enqueue(int val)
+{
+    struct node* n=(struct node*)malloc(sizeof(struct node));
+    if(n==NULL)
+    {
+        printf("Queue is full");
+    }
+    else
+    {
+      n->data=val;
+      n->next=NULL;
+      if(f==NULL)
+      {
+        f=r=n;
+      }
+      else
+      {
+        r->next=n;
+        r=n;
+      }
+    }
+}
+int linkedlisttraversal(struct node* ptr)
+{
+    while(ptr!=NULL)
+    {
+        printf("Element %d ",ptr->data);
+        ptr=ptr->next;
+    }
+}
+int main()
+{
+    linkedlisttraversal(f);
+    enqueue(34);
+    enqueue(45);
+    enqueue(78);
+    linkedlisttraversal(f);
+
+}
