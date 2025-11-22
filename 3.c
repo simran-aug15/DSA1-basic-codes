314739755195ba6360edcbf06fad6bb16445903c
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2025-11-22 19:42:22 +0530

    Some changes

---
+++ 3.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+    int data;
+    struct node* left;
+    struct node* right;
+};
+struct node* Createnode(int data)
+{
+    struct node* n=(struct node*)malloc (sizeof(struct node));
+    n->data=dat;
+    n->left=NULL:
+    n->right=NULL;
+    return n;
+
+}
+int insert(struct node* root,int key)
+{
+    struct node* prev=NULL;
+    while(root!=NULL)
+    {
+        prev=root;
+        if(key==root->data)
+        {
+            return;
+        }
+        else if(key<root->data)
+        {
+            root=root->left;
+        }
+        else
+        {
+            root=root->right;
+        }
+    }
+    struct node* new=Createnode(key);
+    if(key<prev->data)
+    {
+        prev->left=new;
+    }
+    else
+    {
+        prev->right=new;
+    }
+}
+int main()
+{
+    struct node* p=Createnode(5);
+    struct node* p1=Createnode(54);
+    struct node* p2=Createnode(67);
+    struct node* p3=Createnode(50);
+    struct node* p4=Createnode(51);
+    p->left=p1;
+    p->right=p2;
+    p1->left=p3;
+    p1->right=p4;
+    p2->left=p5;
+    insert(p,7);
+    printf("%d ",p->right->right);
+    return 0;
+
+
+}
+
