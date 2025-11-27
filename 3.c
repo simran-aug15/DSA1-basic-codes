9a4a9e4a981fd4770ed57df86169cd94cb10879d
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2025-11-27 10:44:45 +0530

    Some changes

---
+++ 3.c
@@ -0,0 +1,84 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct queue
+{
+    int size;
+    int f,r;
+    int* arr;
+};
+int isfull(struct queue* q)
+{
+    if(q->r==q->size-1)
+    {
+        return 1;
+    }
+    return 0;
+}
+void enqueue(struct queue* q,int val)
+{
+    if(isfull(q))
+    {
+        printf("This queue is full");
+    }
+    else{
+        q->r++;
+        q->arr[q->r]=val;
+        printf(" The values is %d \n",val);
+    }
+}
+int isempty(struct queue* q)
+{
+    if(q->r==q->f)
+{
+    return 1;
+}
+return 0;
+
+}
+int dequeue(struct queue* q)
+{
+    int a=-1;
+    if(isempty(q))
+    {
+        printf("This queue is empty");
+    }
+    else
+    {
+       q->f++;
+       a=q->arr[q->f];
+       return a;
+    }
+}
+int main()
+{
+    int u;
+    int i=0;
+    int visited [7]={0,0,0,0,0,0,0};
+    int a[7][7]={
+    {0,1,1,1,0,0,0};
+    {1,0,0,1,0,0,0};
+    {1,1,0,1,1,0,0};
+    {1,0,1,0,1,0,0};
+    {0,0,1,1,0,1,1};
+    {0,0,0,0,1,0,0};
+    {0,0,0,0,1,0,0};
+         };
+   printf("%d",i);
+   visited[i]=1;
+   enqueue(&q,i);
+   while(isempty(&q));
+   {
+     int u=dequeue(&q);
+   }
+   for(int j=0;j<7;j++)
+   {
+    if(a[u][j]==1 &&visited[j]==0)
+    {
+        printf("%d",j);
+        visited[j]=i;
+        enqueue(&q,j);
+    }
+   }
+
+
+}
