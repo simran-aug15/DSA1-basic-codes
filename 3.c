6a2fed53963e72262b3174d467e46bb7232b2d67
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2025-11-28 11:52:59 +0530

    Some changes

--- 3.c
+++ 3.c
@@ -12,11 +12,11 @@
          };
  void DFS(int i)
  {
-    printf("%d \n";i);
+    printf("%d \n",i);
     visited[i]=1;
     for(int j=0;j<7;j++)
     {
-        if(A[i][j]==1 && !visited[j])
+        if(a[i][j]==1 && !visited[j])
         {
             DFS(j);
         }
