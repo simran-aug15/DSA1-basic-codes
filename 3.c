a54bbf3095cd3f241adbf005c14d8931939a3c4f
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2025-11-30 23:44:10 +0530

    Some changes

--- 3.c
+++ 3.c
@@ -1,19 +1,6 @@
 #include<stdio.h>
-int insertionsort(int arr[],int size)
-{
-    int i,j,key;
-    for(i=1;i<size;i++)
-    {
-        key=arr[i];
-        j=i-1;
-        while(j>=0 && arr[j]>key)
-        {
-            arr[j+1]=arr[j];
-            j=j-1;
-        }
-        arr[j+1]=key;
-    }
-}
+
+
 int selectionsort(int arr[],int size)
 {  int min ,i,temp;
     for(int i=0;i<size-1;i++)
@@ -48,7 +35,7 @@ int insertionsort(int arr[],int size)
     }
 }
 int main()
-    {
+    {   int pass,hold,i;
         int arr[]={2,56,34,78,12};
         int size=sizeof(arr)/sizeof(arr[0]);
         printf("Elements before sorting  ");
@@ -58,13 +45,24 @@ int main()
             
         }
         printf("\n");
-        insertionsort(arr,size);
-        printf("Element after sorting ");
-        for(int i=0;i<size;i++)
-        {    
-            printf("%d  ",arr[i]);
+        for(pass=1;pass<10;pass++)
+    {
+        for(i=0;i<10-1;i++)
+        {
+            if(a[i]>a[i+1])
+            {
+                hold=a[i];
+                a[i]=a[i+1];
+                a[i+1]=hold;
+            }
         }
-        printf("\n");
+    }
+    printf("Item in ascending order ");
+    for(i=0;i<10;++i)
+    {
+        printf("%d  ",a[i]);
+    }
+      printf("\n");
         selectionsort(arr,size);
         printf("Elements after ");
         for(int i=0;i<size;i++)
